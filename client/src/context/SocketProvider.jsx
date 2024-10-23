2689fc06b1d3d34f63a7b3b209e83b07986d9930
Ashutosh <aa8781@srmist.edu.in>
2024-10-23 19:30:46 +0530

    HELPP ME

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -1,26 +1,19 @@
-import React, { createContext, useMemo, useContext } from 'react';
-import { io } from 'socket.io-client';
-import axios from 'axios'
+import React, { createContext, useMemo, useContext } from "react";
+import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-    const socket = useContext(SocketContext);
-    return socket;
+  const socket = useContext(SocketContext);
+  return socket;
 };
 
 export const SocketProvider = (props) => {
-    // Determine the socket server URL based on the environment
-    const socketUrl = process.env.NODE_ENV === 'production'
-        ? 'https://react-video-chat-rtc-1.onrender.com' // Change this to your actual production URL
-        : 'http://localhost:8000'; // Development URL
+  const socket = useMemo(() => io("https://react-video-chat-rtc-1.onrender.com"), []);
 
-    const socket = useMemo(() => io(socketUrl), [socketUrl]);
-    axios.defaults.withCredentials = true
-
-    return (
-        <SocketContext.Provider value={socket}>
-            {props.children}
-        </SocketContext.Provider>
-    );
+  return (
+    <SocketContext.Provider value={socket}>
+      {props.children}
+    </SocketContext.Provider>
+  );
 };
--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -121,8 +121,8 @@ const RoomPage = () => {
           <ReactPlayer
             playing
             muted
-            height="200px"
-            width="500px"
+            height="100px"
+            width="200px"
             url={myStream}
           />
         </>
@@ -133,8 +133,8 @@ const RoomPage = () => {
           <ReactPlayer
             playing
             muted
-            height="200px"
-            width="500px"
+            height="100px"
+            width="200px"
             url={remoteStream}
           />
         </>
